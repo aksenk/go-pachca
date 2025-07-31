369ebeb4f92e103af0c804106faa2b15cc1c3f5a
Николай Аксенов <nk_aksenov@betboom.org>
2025-08-01 00:47:19 +0300

    fix api response errors

--- errors.go
+++ errors.go
@@ -1,9 +1,13 @@
 package pachca
 
-type ErrorResponse struct {
+type APIError struct {
 	Key     string      `json:"key"`
 	Value   string      `json:"value"`
 	Message string      `json:"message"`
 	Code    string      `json:"code"`
 	Payload interface{} `json:"payload"`
+}
+
+type ErrorResponse struct {
+	Errors []APIError `json:"errors"`
 }
