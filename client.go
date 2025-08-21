6369a9013f87810ed1486258e18f33a85fa44307
Николай Аксенов <nk_aksenov@betboom.org>
2025-08-21 17:30:39 +0300

    recover

--- client.go
+++ client.go
@@ -112,6 +112,12 @@ func NewClient(options *ClientOptions) (*Client, error) {
 		SetRetryMaxWaitTime(options.RetryMaxWait).
 		AddRetryCondition(
 			func(r *resty.Response, err error) bool {
+				defer func() {
+					if rec := recover(); rec != nil {
+						fmt.Printf("panic in retry hook: %+v (Request=%+v, Response=%+v)\n", rec, r.Request, r)
+					}
+				}()
+
 				if err != nil {
 					var netErr net.Error
 					if errors.As(err, &netErr) && netErr.Timeout() {
@@ -133,14 +139,14 @@ func NewClient(options *ClientOptions) (*Client, error) {
 			}).
 		AddRetryHook(
 			func(r *resty.Response, err error) {
+				defer func() {
+					if rec := recover(); rec != nil {
+						fmt.Printf("panic in retry hook: %+v (Request=%+v, Response=%+v)\n", rec, r.Request, r)
+					}
+				}()
 				// Если есть наблюдатель, вызываем его с метаданными,
 				// чтобы можно было отслеживать попытки ретрая со стороны приложения
 				if options.RetryObserver != nil {
-					defer func() {
-						if rec := recover(); rec != nil {
-							fmt.Printf("panic in retry hook: %+v (Request=%+v, Response=%+v)\n", rec, r.Request, r)
-						}
-					}()
 					meta := RetryMeta{
 						Error: err,
 					}
